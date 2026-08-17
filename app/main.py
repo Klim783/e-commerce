41694b93b4a8a909571f9988b55a67bd9cb764ee
Cuschevici Climent <kuskevichklim@mail.ru>
2026-08-17 14:38:54 +0300

    add auth router

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
+from enum import auto
+
+from IPython.core.release import author_email
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.v1.auth import router as auth_router
 
 app = FastAPI(title = "E-commerce")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
 )
+
+app.include_router(auth_router, prefix = '/api/v1', tags = ['Auth'])
+
 
 @app.get('/health')
 def health_check():
