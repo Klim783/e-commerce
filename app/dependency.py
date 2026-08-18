abda8f05e8058344da24406530b9c2dc2dc11ff9
Cuschevici Climent <kuskevichklim@mail.ru>
2026-08-18 17:09:04 +0300

    add require_admin function

--- app/dependency.py
+++ app/dependency.py
@@ -32,3 +32,7 @@ def get_current_user(
 	return user
 
 
+def require_admin(current_user:User = Depends(get_current_user)) -> User:
+	if current_user.role != UserRole.ADMIN:
+		raise HTTPException(status_code=403, detail='Admin access required')
+	return current_user
