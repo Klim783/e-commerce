46df3b17a52f87fb27ebf6101e6e4f56a94b78dd
Cuschevici Climent <kuskevichklim@mail.ru>
2026-08-19 14:50:50 +0300

    add some more schemas for products

--- app/schemas/products.py
+++ app/schemas/products.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from decimal import Decimal
-
+from enum import Enum
 from pydantic import BaseModel, Field, field_validator
 
 class CategoryCreateRequest(BaseModel):
@@ -58,3 +58,27 @@ class ProductResponse(BaseModel):
 	is_active:bool
 	category_id:int
 	created_at:datetime
+
+class ProductSortBy(str,Enum):
+	price_asc = "price_asc"
+	price_desc = "price_desc"
+	newest = "newest"
+	rating = "rating"
+
+
+class ProductFilterParams(BaseModel):
+	q:str|None = None
+	category_id :int|None = None
+	min_price:Decimal|None = None
+	max_price:Decimal|None = None
+	min_rating:float|None = None
+	sort_by : ProductSortBy = ProductSortBy.newest
+	page:int = Field(default=1, ge=1)
+	page_size:int = Field(default = 20, ge=1, le=1)
+
+class PaginatedProductsResponse(BaseModel):
+	items:list[ProductResponse]
+	total:int
+	page:int
+	page_size:int
+	total_page:int
