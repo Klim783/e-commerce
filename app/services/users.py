819f972e505dd2c6a4be34f6167d3598fce155b4
Cuschevici Climent <kuskevichklim@mail.ru>
2026-08-20 16:36:16 +0300

    fix bugs

--- app/services/users.py
+++ app/services/users.py
@@ -13,10 +13,9 @@ def register(db:Session, email:str, password:str, full_name:str) -> UserResponse
 	user = users_repository.create_user(db, email, hash_password(password), full_name)
 	return UserResponse.model_validate(user)
 
-def login(db :Session, email:str, password:str)->TokenResponse:
-	user = users_repository.get_user_by_email(db, email)
-	if not user or not verify_password(password, user.hashed_password):
-		raise HTTPException(status_code = 400, detail = 'Invalid email or password')
-	token = create_access_token(user.email)
-	return TokenResponse.model_validate(token)
-
+def login(db: Session, email: str, password: str) -> TokenResponse:
+    user = users_repository.get_user_by_email(db, email)
+    if not user or not verify_password(password, user.hashed_password):
+        raise HTTPException(status_code=400, detail='Invalid email or password')
+    token = create_access_token(user.email)
+    return TokenResponse(access_token=token, token_type="bearer")
